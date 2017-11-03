0e1c5d899a91210d25e6a69dac72d4b413993247
Yoran <yoran_nelissen@hotmail.com>
2017-11-04 00:01:57 +0100

    Getting data from API

---
+++ App/Backend API/initial/src/main/java/hello/controllers/ShopController.java
@@ -0,0 +1,22 @@
+package hello.controllers;
+
+import hello.data.AttackList;
+import hello.data.ItemList;
+import hello.data.SpeciesList;
+import hello.domainclasses.Shop;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 11500809 on 3/11/2017.
+ */
+@RestController
+public class ShopController {
+    Shop shop = new Shop(ItemList.getList(), AttackList.getAttacksList(), SpeciesList.getAllSpecies());
+
+    @RequestMapping("/shop")
+    public Shop shopList(){
+        return shop;
+    }
+}
